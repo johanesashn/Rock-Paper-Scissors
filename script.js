772d38b2cf7f73d5a38a6bbe635a378cb86b4a47
johanesashn <113593955+johanesashn@users.noreply.github.com>
2023-06-28 11:02:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
 const info = document.getElementById('info');
-const userTotal = document.querySelector('.user-total h5')
-const computerTotal = document.querySelector('.computer-total h5')
-const refresh = document.querySelector('.refresh h5')
-const user = document.querySelectorAll('.user ul li img');
+const userTotal = document.querySelector('.user-total p')
+const computerTotal = document.querySelector('.computer-total p')
+const user = document.querySelectorAll('.user-hand button');
 const imgComputer = document.querySelector('.computer img');
 
 function getComputer() {
@@ -19,16 +18,29 @@ function getComputer() {
     if (comp > .9 && comp <= 1) return 'scissors'
 }
 
+function getComputerImage(computer){
+    if (computer === "paper") return "computerPaper"
+    if (computer === "scissors") return "computerScissors"
+    if (computer === "rock") return "computerRock"
+}
+
+function getUserImage(user){
+    if (user === "paper") return "userPaper"
+    if (user === "scissors") return "userScissors"
+    if (user === "rock") return "userRock"
+}
+
 function getResult(user, comp){
+    console.log(user + " " + comp)
     if (user == comp) return 'DRAW';
     if (user == 'rock') return comp == 'scissors' ? 'WIN' : 'LOSE';
     if (user == 'paper') return comp == 'rock' ? 'WIN' : 'LOSE';
     if (user == 'scissors') return comp == 'paper' ? 'WIN' : 'LOSE';
 }
 
-function spin(){
+function computerSpin(){
     const imgComputer = document.querySelector('.computer img')
-    const images = ['rock', 'paper', 'scissors'];
+    const images = ['computerPaper', 'computerRock', 'computerScissors'];
     let i = 0;
     const start = new Date().getTime();
     
@@ -43,47 +55,57 @@ function spin(){
     }, 50)
 }
 
+function userSpin(userImage){
+    const imgComputer = document.querySelector('.user img')
+    const images = ['userRock', 'userPaper', 'userScissors'];
+    let i = 0;
+    const start = new Date().getTime();
+    
+    setInterval(function(){
+        if (new Date().getTime() - start > 800) {
+            clearInterval;
+            return
+        }
+        imgComputer.setAttribute('src', 'img/' + images[i++] + '.png')
+        if (i == images.length) i = 0;
+        
+    }, 100)
+    setTimeout(() => {
+        imgComputer.setAttribute('src', 'img/' + userImage + '.png')
+    }, 801);
+}
+
 let userScore = 0;
 let computerScore = 0;
-
-refresh.addEventListener('click', function(){
-    computerScore = 0
-    userScore = 0
-    computerTotal.innerHTML = computerScore
-    userTotal.innerHTML = userScore
-    info.innerHTML = 'CHOOSE'
-    info.style.color = "black"
-})
-
 
 user.forEach(function(u){  
     u.addEventListener('click', function(){
         const computerChoice = getComputer();
-        const userChoice = u.className;
+        const userChoice = u.id;
         const result = getResult(userChoice, computerChoice);
+        const computerImage = getComputerImage(computerChoice)
+        const userImage = getUserImage(userChoice)
+        console.log(userImage)
     
-        spin();
+        computerSpin();
+        userSpin(userImage);
         
         setTimeout(function(){
-            
-            imgComputer.setAttribute('src', 'img/' + computerChoice + '.png');    
-
+            imgComputer.setAttribute('src', 'img/' + computerImage + '.png');    
             if (result == 'DRAW') {
                 info.innerHTML = "DRAW"
-                info.style.color = 'blue';
+                info.style.color = "white"
             } else if (result == 'LOSE') {
-                info.innerHTML = "YOU LOSE"
-                info.style.color = 'red';
+                info.innerHTML = "LOSE"
+                info.style.color = "red"
                 computerScore += 1;
                 computerTotal.innerHTML = computerScore
             } else {
-                info.innerHTML = "YOU WIN"
-                info.style.color = 'GREEN';
+                info.innerHTML = "WIN"
+                info.style.color = "#03C04A"
                 userScore += 1;
                 userTotal.innerHTML = userScore
             }
-            
-        }, 800)
-        
+        }, 800)     
     })
 })
